a1782c86b61f538e4cdde6df2c0f14e078eddee4
Jessica Huynh <jhuynh@ualberta.ca>
2017-02-17 12:20:45 -0700

    created data gen and driver file

---
+++ DataGenerator.java
@@ -0,0 +1,14 @@
+// The data generator must accept an output filename and the number of
+// integer values to be generated as command-line arguments. Integer values are to be
+// randomly generated. This program is to be written entirely in Java.
+
+package project1;
+import java.io.*;
+
+public class DataGenerator 
+{
+	public DataGenerator()
+	{
+		System.out.println("Data Gen");
+	}
+}
---
+++ Driver.java
@@ -0,0 +1,10 @@
+
+import project1.*;
+
+public class Driver
+{
+	public static void main(String[] args) 
+	{
+		project1.DataGenerator dataGen = new project1.DataGenerator();
+	}
+}
