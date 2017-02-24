d729bb5021eb148802792e6f8b5a5e135c7f8e17
Jessica Huynh <jhuynh@ualberta.ca>
2017-02-23 18:28:43 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,6 +14,7 @@ javac *.java
 # produce header file for C native method
 javah SecondaryInsertionSort
 
+# http://mrjoelkemp.com/2012/01/getting-started-with-jni-and-c-on-osx-lion/
 #### if you are on a linux machine please use the following line instead
 # gcc -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -shared -fpic -o libinsertionsort.so lib_insertionsort.c
 gcc -I$JAVA_HOME/include/ -I$JAVA_HOME/include/darwin/ -dynamiclib -o libinsertionsort.jnilib lib_insertionsort.c
