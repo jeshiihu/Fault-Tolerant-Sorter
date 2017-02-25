7b4f0e35b629ec6939d1fb6d98f73543c4401b28
Jessica Huynh <jhuynh@ualberta.ca>
2017-02-25 12:38:36 -0700

    testing out some things of where to put estuffff

--- DataSorter.java
+++ DataSorter.java
@@ -2,13 +2,19 @@
 // for primary and backup variants, and time limit
 import java.io.*;
 import java.lang.*;
-
+import Helper.*;
 
 public class DataSorter
 {
 	public static void main(String[] args) 
 	{
-		// try primary then call c function	
+		// try primary then call c function
+		Helper.UserIOManager ioManager = new Helper.UserIOManager();
+		Helper.FileManager fManager = new Helper.FileManager();
+
+		String fin = ioManager.getFilenameInput("Enter input filename: ", "txt");
+		if(!fManager.fileExists(fin))
+
 		SecondaryInsertionSort secondarySort = new SecondaryInsertionSort();
 		System.loadLibrary("insertionsort");
 		int i = 2;
--- PrimaryHeapSort.java
+++ PrimaryHeapSort.java
@@ -1,10 +1,26 @@
 // primary variant
 // heap sort
 package SortingAlgorithms;
+import java.io.*;
+import java.lang.*;
+import Helper.*;
+
 
 public class PrimaryHeapSort
 {
+	String _fin;
+	String _fout;
+	
 	public PrimaryHeapSort()
+	{
+		Helper.UserIOManager ioManager = new Helper.UserIOManager();
+		Helper.FileManager fManager = new Helper.FileManager();
+
+		String fin = ioManager.getFilenameInput("Enter input filename: ", "txt");
+		// if(!fManager.fileExists(fin))
+	}
+
+	public void sort()
 	{
 
 	}
